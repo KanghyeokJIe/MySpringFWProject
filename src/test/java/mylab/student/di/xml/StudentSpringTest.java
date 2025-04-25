b8640ca9d8833455ea1ba45dbf17391c7743089e
kanghyeokjie <kanghyeokjie@gmail.com>
2025-04-25 14:39:48 +0900

    course test update

--- src/test/java/mylab/student/di/xml/StudentSpringTest.java
+++ src/test/java/mylab/student/di/xml/StudentSpringTest.java
@@ -1,11 +1,24 @@
 package mylab.student.di.xml;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:mylab-student-di.xml")
 public class StudentSpringTest {
-
+	@Autowired
+	Course course;
+	
+	@Test
+	void testCourse() {
+		assertNotNull(course);
+		assertEquals("Java Programming", course.getCourseName());
+	}
 }
